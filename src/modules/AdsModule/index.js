5b747f57158d2c251e239ce38e0d65bf5dc4e921
Philip <p208p2002@gmail.com>
2020-02-01 14:21:40 +0800

    no message

--- src/modules/AdsModule/index.js
+++ src/modules/AdsModule/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './index.css'
-// import AdSense from 'react-adsense';
+import AdSense from 'react-adsense';
 function Index() {
     return (
         <div id="ADS">
-            {/* <AdSense.Google
-                client='ca-pub-7292810486004926'
-                slot='7806394673'
-            /> */}
+            <AdSense.Google
+                client='ca-pub-3857728160074264'
+                slot='2770270783'
+            />
         </div>
     )
 }
